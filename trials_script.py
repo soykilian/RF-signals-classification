cd8ea4f0bd677cb1967a523846383739ca8dda53
soykilian <mariavdacosta.27@gmail.com>
2022-07-22 12:14:15 +0200

    cosas

--- trials_script.py
+++ trials_script.py
@@ -1,4 +1,5 @@
 import h5py
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, optimizers
@@ -10,8 +11,10 @@ import scipy.io as io
 from typing import Any, Dict
 
 Array = np.ndarray
-dataset_path = '/mnt/Data/gmr/Dataset/'
-path = '/mnt/Data/gmr/models/'
+path = '/home/usuario_gmr/gmr/'
+n_epochs = 500
+batch_size = 500
+dataset_path = path
 with h5py.File(dataset_path + 'X_train.mat', 'r') as f:
     X_train = np.array(f['X_train']).T
 print(X_train.shape)
@@ -28,6 +31,8 @@ Y_train = io.loadmat(dataset_path + 'Y_train.mat')
 Y_train = Y_train['Y_train']
 Y_test = io.loadmat(dataset_path + 'Y_test.mat')
 Y_test = Y_test['Y_test']
+print(Y_train.shape)
+print(Y_test.shape)
 classes = ['LFM', '2FSK', '4FSK', '8FSK', 'Costas', '2PSK', '4PSK', '8PSK', 'Barker', 'Huffman', 'Frank', 'P1', 'P2',
            'P3', 'P4', 'Px', 'Zadoff-Chu', 'T1', 'T2', 'T3', 'T4', 'NM', 'ruido']
 I_x = X_train[:, :, 0]
@@ -36,8 +41,8 @@ X_train[:, :, 1] = np.arctan(Q_x, I_x) / np.pi
 X_train[:, :, 0] = np.abs(I_x + 1j * Q_x)
 I_t = X_test[:, :, 0]
 Q_t = X_test[:, :, 1]
-X_test[:, :, 0] = np.arctan(Q_t, I_t) / np.pi
-X_test[:, :, 1] = np.abs(I_t + 1j * Q_t)
+X_test[:, :, 1] = np.arctan(Q_t, I_t) / np.pi
+X_test[:, :, 0] = np.abs(I_t + 1j * Q_t)
 
 np.random.seed(2022)
 X_train, Y_train, lbl_train = sklearn.utils.shuffle(X_train[:], Y_train[:], lbl_train[:], random_state=2022)
@@ -48,7 +53,9 @@ print("---------------------------------------------------")
 
 def lstm_network(input_shape: int) -> Model:
     X_input = Input(input_shape)
-    X = LSTM(128, return_sequences=True, name='lstm0', dropout=0.3)(X_input)
+    #dropout = Dropout(0.3, input_shape=X_input.shape)
+    #X = dropout(X_input)
+    X = LSTM(128, return_sequences=True, name='lstm0')(X_input)
     X = LSTM(128, name='lstm1')(X)
     X = Dense(23, activation='softmax', name='fc0')(X)
     model = Model(inputs=X_input, outputs=X)
@@ -63,18 +70,100 @@ model.compile(optimizer=optimizers.Adam(1e-4), loss='categorical_crossentropy', 
 print("Optimizer added.")
 print("---------------------------------------------------")
 
-def Train(model: Model, data: Dict, n_epochs: int = 500, batch_size: int = 150):
-    history = model.fit(data['X_train'], data['Y_train'], epochs=n_epochs, batch_size=batch_size, callbacks=c,
-                        validation_data=(data['X_test'], data['Y_test']))
+train = True
+if train:
+    history = model.fit(X_train, Y_train, epochs=n_epochs, batch_size=batch_size, callbacks=c, validation_data=(X_test, Y_test))
     with open(path + 'history_rnn.json', 'w') as f:
         json.dump(history.history, f)
     model_json = model.to_json()
     with open(path + 'model_rnn.json', 'w') as json_file:
         json_file.write(model_json)
-
-def save_best():
+else:
     model.load_weights(path + 'best_model.h5')
     with open(output_path + 'history_rnn.json', 'r') as f:
         history = json.load(f)
 
-Train(model, {'X_train': X_train, 'X_test': X_test, 'Y_train': Y_train, 'Y_test': Y_test})
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['test', 'val'])
+plt.show()
+plt.savefig(path + '\graphs\model_loss.pdf')
+
+def getConfusionMatrixPlot(true_labels, predicted_labels,title):
+    # Compute confusion matrix
+    cm = confusion_matrix(true_labels, predicted_labels)
+    cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    cm_norm = np.nan_to_num(cm_norm)
+    cm = np.round(cm_norm,2)
+    print(cm)
+
+    # create figure
+    width = 18
+    height = width / 1.618
+    fig = plt.figure(figsize=(width, height))
+    plt.clf()
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    ax.set_xlabel('Predicted label')
+    ax.set_ylabel('True label')
+    res = ax.imshow(cm, cmap=plt.cm.binary,
+                    interpolation='nearest', vmin=0, vmax=1)
+
+    # add color bar
+    plt.colorbar(res)
+
+    # annotate confusion entries
+    width = len(cm)
+    height = len(cm[0])
+
+    for x in range(width):
+        for y in range(height):
+            ax.annotate(str(cm[x][y]), xy=(y, x), horizontalalignment='center',
+                        verticalalignment='center', color=getFontColor(cm[x][y]))
+
+    # add genres as ticks
+    alphabet = classes
+    plt.xticks(range(width), alphabet[:width], rotation=30)
+    plt.yticks(range(height), alphabet[:height])
+    plt.title(title)
+    return plt
+
+def getFontColor(value):
+    if np.isnan(value):
+        return "black"
+    elif value < 0.2:
+        return "black"
+    else:
+        return "white"
+
+acc={}
+snrs = [-12,-10,-8,-6,-4,-2,0,2,4,6,8,10,12,14,16,18,20]
+for snr in snrs:
+    test_SNRs = list(map(lambda x: lbl_test[x][1], range(0,X_test.shape[0])))
+    test_X_i = X_test[[i for i,x in enumerate(test_SNRs) if x==snr]]
+    test_Y_i = Y_test[[i for i,x in enumerate(test_SNRs) if x==snr]]
+    # estimate classes
+    test_Y_i_hat = np.array(model.predict(test_X_i))
+    width = 18
+    height = width / 1.618
+    plt.figure(figsize=(width, height))
+    plt = getConfusionMatrixPlot(np.argmax(test_Y_i, 1), np.argmax(test_Y_i_hat, 1),title="ResNet Confusion Matrix (SNR=%d)"%(snr))
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.savefig(output_path + '\graphs\confmat_'+str(snr)+'.pdf')
+    conf = np.zeros([len(classes),len(classes)])
+    confnorm = np.zeros([len(classes),len(classes)])
+    for i in range(0,test_X_i.shape[0]):
+        j = list(test_Y_i[i,:]).index(1)
+        k = int(np.argmax(test_Y_i_hat[i,:]))
+        conf[j,k] = conf[j,k] + 1
+    for i in range(0,len(classes)):
+        confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+    plt.figure()
+    cor = np.sum(np.diag(conf))
+    ncor = np.sum(conf) - cor
+    print("Overall Accuracy: ", cor / (cor+ncor))
+    acc[snr] = 1.0*cor/(cor+ncor)
+
