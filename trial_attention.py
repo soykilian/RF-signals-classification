cf6402f48b24b2c0584938768b419143eff587aa
soykilian <mariavdacosta.27@gmail.com>
2022-09-03 15:10:03 +0200

    second trials

--- trial_attention.py
+++ trial_attention.py
@@ -34,32 +34,84 @@ Y_test = Y_test['Y_test']
 print(Y_test.shape)
 classes = ['LFM', '2FSK', '4FSK', '8FSK', 'Costas', '2PSK', '4PSK', '8PSK', 'Barker', 'Huffman', 'Frank', 'P1', 'P2',
            'P3', 'P4', 'Px', 'Zadoff-Chu', 'T1', 'T2', 'T3', 'T4', 'NM', 'ruido']
-I_x = X_train[:, :, 0]
-Q_x = X_train[:, :, 1]
-X_train[:,:,1] = np.arctan(Q_x, I_x)/ np.pi
-X_train[:,:,0] = np.abs(I_x + 1j*Q_x)
-I_t = X_test[:, :, 0]
-Q_t = X_test[:, :, 1]
-X_test[:, :, 0] = np.arctan(Q_t, I_t)/np.pi
-X_test[:, :, 1] = np.abs(I_t + 1j*Q_t)
+AF = False
+if AF:
+
+    I_tr = X_train[:, :, 0]
+    Q_tr = X_train[:, :, 1]
+    X_tr = I_tr + 1j * Q_tr
+
+    X_train[:, :, 1] = np.arctan2(Q_tr, I_tr) / np.pi
+    X_train[:, :, 0] = np.abs(X_tr)
+
+    I_te = X_test[:, :, 0]
+    Q_te = X_test[:, :, 1]
+    X_te = I_te + 1j * Q_te
+
+    X_test[:, :, 1] = np.arctan2(Q_te, I_te) / np.pi
+    X_test[:, :, 0] = np.abs(X_te)
+
+    del I_tr
+    del Q_tr
+    del X_tr
+    del I_te
+    del Q_te
+    del X_te
+
 np.random.seed(2022)
 X_train, Y_train, lbl_train = sklearn.utils.shuffle(X_train[:],Y_train[:], lbl_train[:], random_state=2022)
 X_test, Y_test, lbl_test = sklearn.utils.shuffle(X_test[:], Y_test[:], lbl_test[:], random_state=2022)
+"""
+class AttentionBlock():
+    def init(self, 
+             num_heads : int = 2,
+             ff_dim = None,
+             dropout: int = 0.1,
+             key_dim : int = 2):
+        self.attention_layer = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=key_dim)
+        self.attention_dropout = keras.layers.Dropout(dropout)
+        self.attention_norm = keras.layers.LayerNormalization(epsilon=1e-6)
+        self.conv1 = keras.layers.Conv1D(filters=ff_dim, kernel_size=1, activation='relu')
+        self.dropout = keras.layers.Dropout(dropout)
+        self.norm = keras.layers.LayerNormalization(epsilon=1e-6)
+        
+    def call(self, input_shape):
+        x_input = Input(input_shape)
+        print("Input tensor", x_input)
+        x = self.attention_layer(x_input, x_input)
+        x = self.attention_dropout(x) 
+        x = self.attention_norm(x)
+        x = self.conv1(x)
+        x = self.dropout(x)
+        x = self.norm(x)
+        print("Output tensor", x)
+        model = Model(inputs=x_input, outputs=x)
+        model.summary()
+        return model
+        """
 
 def ModelTrunk(input_shape : int):
     X_input = tf.keras.Input(input_shape)
     num_layers = 5
-    #attention_layers = [AttentionBlock(num_heads=2, head_size=128, ff_dim=None, dropout=0.1) for _ in range (num_layers)]
-    #for layer in attention_layers :
-      #  x = layer(x)
     attention_block = tf.keras.layers.MultiHeadAttention(num_heads=2, key_dim=2)
     x = attention_block(X_input,X_input)
-    x = Flatten()(x)
-    x = Dense(128, activation='selu')(x)
-    x = AlphaDropout(0.6)(x)
-    x = Dense(128, activation='selu')(x)
-    x = AlphaDropout(0.6)(x)
-    x = Dense(23, activation='softmax')(x)
+    attention_dropout = keras.layers.Dropout(0.1)
+    x= attention_dropout(x)
+    attention_norm = keras.layers.LayerNormalization(epsilon=1e-6)
+    x = attention_norm(x)
+    conv1 = keras.layers.Conv1D(filters=None, kernel_size=1, activation='relu')
+    x = conv1(x)
+    dropout = keras.layers.Dropout(0.1)
+    x = dropout(x)
+    norm = keras.layers.LayerNormalization(epsilon=1e-6)
+    x = norm(x)
+    print(x)
+   # x = Flatten()(x)
+   # x = Dense(128, activation='selu')(x)
+   # x = AlphaDropout(0.6)(x)
+   # x = Dense(128, activation='selu')(x)
+   # x = AlphaDropout(0.6)(x)
+   # x = Dense(23, activation='softmax')(x)
     model = Model(inputs = X_input, outputs = x)
     model.summary()
     return model
